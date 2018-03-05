ad0492c22e8bc2dfc1f356e21cc6d6ff90718df1
guitarxuser <35921739+guitarxuser@users.noreply.github.com>
2018-03-05 21:02:03 +0100

    added unistall script

---
+++ unistall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#install within /usr/local/bin
+
+rm /usr/local/bin/tty0tty
+rm /usr/local/bin/rot_enc
+rm /usr/local/bin/rot_enc_dummy
+rm /usr/local/bin/TelescopeServerNexStar
+ls -ltr /usr/local/bin
