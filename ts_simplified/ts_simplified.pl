5405ef1f91f1cd27e4c8110eb22333f4149cc60b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-15 16:58:11 +0100

    reads /dev/tty end sends the ra dec telegram to client

--- ts_simplified/ts_simplified.pl
+++ ts_simplified/ts_simplified.pl
@@ -1,41 +1,4 @@
 #!/usr/local/bin/perl
-
-#---------------------------------------------------------------------
-#        start server on local host 
-#---------------------------------------------------------------------
-#-----------------------
-#server->client:
-#MessageCurrentPosition (type = 0):#
-
-#LENGTH (2 bytes,integer): length of the message
-#TYPE   (2 bytes,integer): 0
-#TIME   (8 bytes,integer): current time on the server computer in microseconds
-#           since 1970.01.01 UT. Currently unused.
-#RA     (4 bytes,unsigned integer): right ascension of the telescope (J2000)
-#           a value of 0x100000000 = 0x0 means 24h=0h,
-#           a value of 0x80000000 means 12h
-#DEC    (4 bytes,signed integer): declination of the telescope (J2000)
-#           a value of -0x40000000 means -90degrees,
-#           a value of 0x0 means 0degrees,
-#           a value of 0x40000000 means 90degrees
-#STATUS (4 bytes,signed integer): status of the telescope, currently unused.
-#           status=0 means ok, status<0 means some error
-#
-#
-# unsigned 8bit  Integer (Char  -> C)
-# unsigned 16bit Integer (Short -> S)
-# signed   32bit Integer (Long  -> l)
-# unsigned 8bit  Integer (Char  -> C)
-# unsigned 64bit Integer (long long  -> Q)
-## Little Endian (X86 Order)
-# on x86 Processors
-#my $out=pack('CSlC',$zahl1,$zahl2,$zahl3,$zahl1);
-# on PPC or other not x86 Processors force Little Endian
-#my $out=pack('C<S<lC',$zahl1,$zahl2,$zahl3,$zahl1);
-
-# Big Endian (Network Order)
-#my $out=pack('C>S>lC',$zahl1,$zahl2,$zahl3,$zahl1);
-
 use strict;
 use IO::Socket;
 #use Time::Local;
@@ -45,6 +8,27 @@ my $client = 0;
 my $REVOL_STEPS_COUNT=256;
 my $MAX_NUMBER=4294967296;
 my $globalCounter=0;
+my @devices;
+#---------------------------------------------------------------------
+#        start processing
+#---------------------------------------------------------------------
+
+if (-e "/tmp/devices.txt")
+  {
+   open (LOGFILE,"< /tmp/devices.txt") or die "could not open logfile : $! \n"  ;
+  
+   foreach    my $line  (<LOGFILE>)
+    {
+     print "$line\n" ;   
+     @devices=split(';',$line);
+    }
+#   print   @devices;  
+}
+close(LOGFILE);
+#synchronisation
+my $serial=$devices[1];
+
+my $cmd_line="cat $serial";
 
 $sock = new IO::Socket::INET(LocalPort => MYPORT,
 		             Reuse     => 1,
@@ -56,10 +40,7 @@ while ($client = $sock->accept())
   {
    print "Accepted connection from ",
    $client->peerhost(), ":", $client->peerport(), "\n";
-my   $cmd_line="rot_enc_for_ts";
 
-
-#print "cmd_line3 $cmd_line3 \n";
 
 open(IN,"$cmd_line|") or die "could not start $cmd_line: $!";
 
@@ -68,23 +49,13 @@ open(IN,"$cmd_line|") or die "could not start $cmd_line: $!";
 foreach   my $line  (<IN>)
 {
 
-        print "telescope line $line\n";
-    			
+        print "tty line $line\n";
+         my $out_calc=ra_dec_time($line);
+    	print $client $out_calc;
 } 
  
    
-    my $ra_calc=($globalCounter/$REVOL_STEPS_COUNT)*$MAX_NUMBER;
-       $ra_calc= sprintf("%08d", $ra_calc);
-       print("counter is $globalCounter $ra_calc\n");
-    my $out_calc=ra_dec_time($ra_calc);   
-       chomp;
-       print $client $out_calc;
-       $globalCounter++;
-       if ($globalCounter==$REVOL_STEPS_COUNT)
-        {
-	 $globalCounter=0;
-        }   
-   
+  
     $client->close() if defined $client;
 }   
 
@@ -93,10 +64,11 @@ sub ra_dec_time()
    my $length=24;
    my $type=0;
    my $time= time();
-#   my $ra =0xBAAAAB00;
-   my $ra_inp = shift(@_);
-   my $ra = $ra_inp;
-   my $dec=0x20670280;
+   my $ra =0xBAAAAB00;
+   my $ra_dec_inp = shift(@_);
+   my @ra_dec==split(',',$ra_dec_inp);
+#   my $ra = $ra_dec[0];
+   my $dec= $ra_dec[1];
    my $status=0;
 
    my $out=pack("SSQLll",$length,$type,$time,$ra,$dec,$status);
